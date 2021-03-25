7b015a16c1479132fa0717bf59026aaeb2481bd1
jackl <jacklong34@gmail.com>
2021-03-25 16:12:04 -0400

    Added Auth0

--- AngularTone/src/app/app-routing.module.ts
+++ AngularTone/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HubComponent } from './Components/pages/hub/hub.component';
 import { InstrumentComponent } from './instrument/instrument.component';
 import { ProfileComponent } from './Components/pages/profile/profile.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
-  {path: '', component: InstrumentComponent, data: { title: 'Home' }},
-  {path: 'hub', component: HubComponent},
-  {path: 'profile', component: ProfileComponent}
+  {
+    path: '', component: InstrumentComponent, data: { title: 'Home' }
+  },
+  {path: 'hub', component: HubComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- AngularTone/src/app/app.module.ts
+++ AngularTone/src/app/app.module.ts
@@ -18,9 +18,11 @@ import { FooterComponent } from './Components/shared/footer/footer.component';
 import { HubComponent } from './Components/pages/hub/hub.component';
 import { ProfileComponent } from './Components/pages/profile/profile.component';
 import { ChatroomComponent } from './Components/pages/chatroom/chatroom.component';
+import { AuthButtonComponent } from './Components/auth-button/auth-button.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
-  declarations: [AppComponent, InstrumentComponent, NavbarComponent, FooterComponent, HubComponent, ProfileComponent, ChatroomComponent],
+  declarations: [AppComponent, InstrumentComponent, NavbarComponent, FooterComponent, HubComponent, ProfileComponent, ChatroomComponent, AuthButtonComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -30,7 +32,14 @@ import { ChatroomComponent } from './Components/pages/chatroom/chatroom.componen
     MatGridListModule,
     MatIconModule,
     MatSliderModule,
-    HttpClientModule
+    HttpClientModule,
+    AuthModule.forRoot(
+      {
+        domain: 'dev-vymugu6z.us.auth0.com',
+        clientId: 'XK3HVI5aAOQMHCNQL5MYtvIgOCg02uKD'
+      }
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent],
